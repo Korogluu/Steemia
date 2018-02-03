3b3ef20c51e56eb3ffe68fb3dbd878cfef2833e8
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-02-03 05:21:49 -0500

    refreshing added and onPush strategy used

--- src/pages/TabsSections/feed/feed.ts
+++ src/pages/TabsSections/feed/feed.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { IonicPage, App } from 'ionic-angular';
 import { Post } from 'models/models';
-import { SteemProvider } from '../../../providers/steem/steem';
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs/Observable";
 import * as fromPostsActions from '../../../app/actions/posts';
@@ -10,18 +9,17 @@ import { State } from '../../../app/reducers';
 @IonicPage()
 @Component({
   selector: 'page-feed',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'feed.html'
 })
 export class FeedPage implements OnInit {
 
-  private contents: Array<Post> = [];
   private perPage = 10;
-  private result: any;
 
   private feedPosts$: Observable<any>;
-  private isLoading$: Observable<any>;
-  constructor(private steemProvider: SteemProvider,
-              private appCtrl: App,
+  private isLoading$: Observable<boolean>;
+
+  constructor(private appCtrl: App,
               private _store: Store<State>) {}
 
 
@@ -29,16 +27,6 @@ export class FeedPage implements OnInit {
     this.feedPosts$ = this._store.select(state => state.posts.data);
     this.isLoading$ = this._store.select(state => state.posts.isLoading);
     this._store.dispatch(new fromPostsActions.fetchFeed({tag:"jaysermendez", limit: this.perPage}));
-  }
-  /**
-   * 
-   * Method to get posts in the user feed
-   * 
-   * @returns Observable with an array of posts
-   * @author Jayser Mendez.
-   */
-  private getFeed(): Observable<Array<Post>> {
-    return this.steemProvider.getFeed({tag:"jaysermendez", limit: this.perPage})
   }
 
   /**
@@ -48,12 +36,12 @@ export class FeedPage implements OnInit {
    * @param {Event} refresher
    */
   private doRefresh(refresher): void {
-    // this.getFeed()
-    // .takeUntil( this.destroyed$ )
-    // .subscribe((data: Array<Post>) => {
-    //   this.contents = data;
-    //   refresher.complete();
-    // });
+    this.perPage = 10
+    this._store.dispatch(new fromPostsActions.fetchFeed({tag:"jaysermendez", limit: this.perPage}));
+    this.isLoading$.subscribe(loading => {
+      if (loading) null // Sleep while it is loading :D
+      else refresher.complete();
+    });
   }
 
   /**
@@ -66,11 +54,9 @@ export class FeedPage implements OnInit {
     this.perPage += 10
     this._store.dispatch(new fromPostsActions.fetchFeed({tag:"jaysermendez", limit: this.perPage}));
     this.isLoading$.subscribe(loading => {
-      if (loading) {
-        // Sleep while it is loading :D
-      } else {
-        infiniteScroll.complete();
-      }
+      if (loading) null // Sleep while it is loading :D
+      else infiniteScroll.complete();
+      
     });
   }
 
